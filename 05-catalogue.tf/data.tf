1c394af710c0b1a9874dbe157cb048141fa9785b
yesdani <yesdaniece@gmail.com>
2025-04-30 11:30:27 +0100

    listener rule added

--- 05-catalogue.tf/data.tf
+++ 05-catalogue.tf/data.tf
@@ -29,6 +29,6 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 data "aws_ssm_parameter" "private_subnet_ids" {
    name = "/${var.project_name}/${var.env}/private_subnet_ids"
 }
-data "aws_ssm_parameter" "aws_lb_listener_arn" {
-   name = "/${var.project_name}/${var.env}/aws_lb_listener_arn"
+data "aws_ssm_parameter" "app_alb_listener_arn" {
+   name = "/${var.project_name}/${var.env}/app_alb_listener_arn"
 }
