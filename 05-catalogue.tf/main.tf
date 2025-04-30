e7ab121e343bc49c9952ae6df5623f11f4207ded
yesdani <yesdaniece@gmail.com>
2025-04-30 19:28:45 +0100

    public ids

--- 05-catalogue.tf/main.tf
+++ 05-catalogue.tf/main.tf
@@ -47,6 +47,7 @@ resource "aws_autoscaling_group" "catalogue" {
   health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 2
+  target_group_arns = [aws_lb_target_group.catalogue.arn]
   launch_template {
     id = aws_launch_template.catalogue.id
     version = "$Latest"
