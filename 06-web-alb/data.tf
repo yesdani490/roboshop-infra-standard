2ba7d6893976db0ec7e5749c527e3efdb968451a
yesdani <yesdaniece@gmail.com>
2025-04-30 15:57:26 +0100

    web alb

--- 06-web-alb/data.tf
+++ 06-web-alb/data.tf
@@ -1,6 +1,9 @@
 data "aws_ssm_parameter" "web_alb_sg_id" {
   name = "/${var.project_name}/${var.env}/web_alb_sg_id"
 }
-data "aws_ssm_parameter" "public_subnet_cidr" {
-  name = "/${var.project_name}/${var.env}/public_subnet_cidr"
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project_name}/${var.env}/public_subnet_ids"
+}
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project_name}/${var.env}/vpc_id"
 }
--- 06-web-alb/main.tf
+++ 06-web-alb/main.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "app_alb" {
   name               = "${var.project_name}-${var.common_tags.Component}"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_ssm_parameter.app_alb_sg_id.value]
-  subnets            = split(",",data.aws_ssm_parameter.public_subnet_cidr.value)
+  security_groups    = [data.aws_ssm_parameter.web_alb_sg_id.value]
+  subnets            = split(",",data.aws_ssm_parameter.public_subnet_ids.value)
 
   #enable_deletion_protection = true
 
