1e674fe272d1e3f145e4b961b9ffe05eecb290d6
yesdani <yesdaniece@gmail.com>
2025-04-30 12:02:29 +0100

    listener rule added

--- 05-catalogue.tf/catalogue.sh
+++ 05-catalogue.tf/catalogue.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 cd /home/centos
-git clone https://github.com/yesdani490/Roboshop-shell-tf.git
+git clone -b main https://github.com/yesdani490/Roboshop-shell-tf.git
 cd Roboshop-shell-tf 
 sh catalogue.sh
