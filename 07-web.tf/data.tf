178e01031cfd98d1e52b12fbdeff6813a2643b7b
yesdani <yesdaniece@gmail.com>
2025-07-11 18:35:30 +0100

    updated web.tf

---
+++ 07-web.tf/data.tf
@@ -0,0 +1,35 @@
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project_name}/${var.env}/vpc_id"
+}
+
+data "aws_ami" "devops-ami" {
+  most_recent      = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Centos-8-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_ssm_parameter" "web_sg_id" {
+   name = "/${var.project_name}/${var.env}/web_sg_id"
+}
+data "aws_ssm_parameter" "private_subnet_ids" {
+   name = "/${var.project_name}/${var.env}/private_subnet_ids"
+} 
+
+
+
+
---
+++ 07-web.tf/web.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+cd /home/centos
+git clone -b master https://github.com/yesdani490/Roboshop-shell-tf.git
+cd Roboshop-shell-tf 
+sh web.sh
